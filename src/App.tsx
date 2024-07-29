e5df16dc52d4acb4f816667d41bb294367fefb75
William Sammarco <149804312+wsammarco-fms@users.noreply.github.com>
2024-07-28 19:37:17 -0700

    feat: add @mui/icons-material package

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,21 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import {CssBaseline} from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <meta name="viewport" content="initial-scale=1, width=device-width"/>
+
+        <img src={logo} className="App-logo" alt="logo"/>
         <a href={"http://sammarco.me"}>sammarco.me</a>
       </header>
+      <body>
+      <CssBaseline />
+
+      </body>
     </div>
   );
 }
