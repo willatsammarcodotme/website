1085568a05c21aa3511b9b9e3a466a98c28d57f0
William Sammarco <149804312+wsammarco-fms@users.noreply.github.com>
2024-07-28 19:05:14 -0700

    feat(netlify): become a netlify pro complete

--- src/App.tsx
+++ src/App.tsx
@@ -11,17 +11,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload check refresh.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href={"http://sammarco.me"}>sammarco.me</a>
       </header>
     </div>
   );
